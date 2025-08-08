effc73e9b64c7c131a58e2ad2cf09281c409e8a7
許閎量 <xuhongliang@xuhongliangde-MacBook-Pro.local>
2025-08-08 21:52:42 +0900

    Add Spring Boot backend project

---
+++ backend/src/test/java/com/example/mvp1/Mvp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mvp1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mvp1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
